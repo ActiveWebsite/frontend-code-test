51fd425e4a8f57981e4cbee398b803a830baedbe
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-19 12:36:07 -0600

    complete challenge 4

--- Javascript/js_test.js
+++ Javascript/js_test.js
@@ -7,18 +7,25 @@ $(document).ready(function() {
 	$("#content li:nth-last-of-type(1)").css("color", "red");
 
 	// challenge 2
-	sally.children().css( "color", "orange" );
+	sally.children().css("color", "orange");
 
 	// challenge 3
 	$("select").on("change", appendSelected)
 
 	function appendSelected() {
-		var targetText = $( "option:selected" ).text()
+		var targetText = $("option:selected").text()
 		var $newDiv = $(`<div class = "new-div">${targetText}</div>`)
 		$(".new-div").remove()
-		if(targetText !=='Choose') {
+		if (targetText !== 'Choose') {
 			$('fieldset').append($newDiv)
 		}
 	}
+
+	// challenge 4
+	(function runner() {
+		var targetHeight = $("#setDims").height();
+		var targetWidth = $("#setDims").width();
+		$("#setDims").after(" I have a height of " + targetHeight + " and a width of " + targetWidth + "!")
+	})();
 
 })
