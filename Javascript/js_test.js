f5afe2f14dad0c4b75ca4bfa20cfdfc2802c396a
Matthew Moyer <matthewrmoyer@gmail.com>
2017-05-19 12:16:28 -0600

    complete challenge 3

--- Javascript/js_test.js
+++ Javascript/js_test.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 
 	var sally = $('li:contains("Sally")');
+	var dropDown = $('select')
 
-
+	// challenge 1
 	$("#content li:nth-last-of-type(1)").css("color", "red");
 
-
+	// challenge 2
 	sally.children().css( "color", "orange" );
+
+	// challenge 3
+	$("select").on("change", function(){
+		var targetText = $( "option:selected" ).text()
+		var $newDiv = $(`<div class = "new-div">${targetText}</div>`)
+		$(".new-div").remove()
+		$('fieldset').append($newDiv)
+	})
 
 
 
