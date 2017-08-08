4f649a85aac2c7d6b5fdf6b2e514997d404c4dc4
Ryan Frazier <ryanpfrazier@gmail.com>
2017-08-08 15:52:12 -0600

    tabs done, dropdown responsive

--- HTML:CSS/js/main.js
+++ HTML:CSS/js/main.js
@@ -1,1 +1,6 @@
-
+$('.nav').on('show.bs.tab', function (e) {
+  let activeTab = e.target.hash.slice(1)
+  $('.nav .active').removeClass('active')
+  $(`.nav .${activeTab}`).addClass('active')
+  $(".dropdown-toggle").html(e.target.text + ' <span class="caret-right caret"></span>')
+})
