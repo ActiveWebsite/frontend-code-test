95882456be249868bec0e5549ead37c2640f132a
Ryan Frazier <ryanpfrazier@gmail.com>
2017-08-08 20:08:43 -0600

    header on mobile done

--- HTML:CSS/js/main.js
+++ HTML:CSS/js/main.js
@@ -4,3 +4,9 @@ $('.nav').on('show.bs.tab', function (e) {
   $(`.nav .${activeTab}`).addClass('active')
   $(".dropdown-toggle").html(e.target.text + ' <img src="./Assets/SVGs/angle-down.svg" alt="">')
 })
+
+$('.top-menu li a').on('click', function (e) {
+  let activeTab = e.target.hash.slice(1)
+  $('.nav-link.active').removeClass('active')
+  $(`.nav-link.${activeTab}`).addClass('active')
+})
