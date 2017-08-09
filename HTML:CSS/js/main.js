78d3e681a9ff6cdc3586142f32b942bf12e0f8a0
Ryan Frazier <ryanpfrazier@gmail.com>
2017-08-08 18:03:39 -0600

    footer good, added some responsiveness to text

--- HTML:CSS/js/main.js
+++ HTML:CSS/js/main.js
@@ -2,5 +2,5 @@ $('.nav').on('show.bs.tab', function (e) {
   let activeTab = e.target.hash.slice(1)
   $('.nav .active').removeClass('active')
   $(`.nav .${activeTab}`).addClass('active')
-  $(".dropdown-toggle").html(e.target.text + ' <span class="caret-right caret"></span>')
+  $(".dropdown-toggle").html(e.target.text + ' <img src="./Assets/SVGs/angle-down.svg" alt="">')
 })
