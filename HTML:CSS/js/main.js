493cb3514bf52decf81da2d9b0202aefce6d20a0
Ryan Frazier <ryanpfrazier@gmail.com>
2017-08-08 21:45:35 -0600

    site complete

--- HTML:CSS/js/main.js
+++ HTML:CSS/js/main.js
@@ -1,12 +1,14 @@
 $('.nav').on('show.bs.tab', function (e) {
-  let activeTab = e.target.hash.slice(1)
-  $('.nav .active').removeClass('active')
-  $(`.nav .${activeTab}`).addClass('active')
+  updateActiveTab(e)
   $(".dropdown-toggle").html(e.target.text + ' <img src="./Assets/SVGs/angle-down.svg" alt="">')
 })
 
 $('.top-menu li a').on('click', function (e) {
+  updateActiveTab(e)
+})
+
+function updateActiveTab(e) {
   let activeTab = e.target.hash.slice(1)
   $('.nav-link.active').removeClass('active')
   $(`.nav-link.${activeTab}`).addClass('active')
-})
+}
